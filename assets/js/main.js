380a75593ee59941ab315457ce892c8be910e801
portal <145818737+entrada11portal@users.noreply.github.com>
2026-02-20 21:40:43 -0300

    Create main.js

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+function openMenu() {
+    document.getElementById("sideMenu").classList.add("active");
+}
+
+function closeMenu() {
+    document.getElementById("sideMenu").classList.remove("active");
+}
+
+const links = document.querySelectorAll(".menu-links a");
+links.forEach(link => {
+    link.addEventListener("click", function() {
+        links.forEach(l => l.classList.remove("active"));
+        this.classList.add("active");
+    });
+});
