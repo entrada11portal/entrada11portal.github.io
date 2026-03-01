2ef4b9db4e242e2979cf1652cbfb01c28d7686d2
portal <145818737+entrada11portal@users.noreply.github.com>
2026-03-01 00:03:57 -0300

    Update sininho.js

--- assets/js/sininho.js
+++ assets/js/sininho.js
@@ -1,10 +1,3 @@
-/*document.getElementById("sininho").addEventListener("click", function() {
-  OneSignalDeferred.push(function(OneSignal) {
-    OneSignal.showSlidedownPrompt();
-  });
-});*/
-
-
 document.addEventListener("DOMContentLoaded", function () {
   const botao = document.getElementById("sininho");
 
@@ -13,6 +6,7 @@ document.addEventListener("DOMContentLoaded", function () {
       window.OneSignalDeferred = window.OneSignalDeferred || [];
       OneSignalDeferred.push(async function (OneSignal) {
         await OneSignal.Slidedown.promptPush();
+        console.log("clicou no sininho");
       });
     });
   }
