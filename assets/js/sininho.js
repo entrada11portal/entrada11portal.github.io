ae267ae416cbdf59742e3bd2f1089dfefd3df268
portal <145818737+entrada11portal@users.noreply.github.com>
2026-02-28 23:43:10 -0300

    Update sininho.js

--- assets/js/sininho.js
+++ assets/js/sininho.js
@@ -1,7 +1,5 @@
-<script>
 document.getElementById("sininho").addEventListener("click", function() {
   OneSignalDeferred.push(function(OneSignal) {
     OneSignal.showSlidedownPrompt();
   });
 });
-</script>
